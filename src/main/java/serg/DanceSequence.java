8132bd818b91e3e43e518e579ece0560cee09bf8
sergey <sergey.krasnjanski@gmail.com>
2017-11-17 13:46:06 +0300

    1-Add-Class-DanceSequence

---
+++ src/main/java/serg/DanceSequence.java
@@ -0,0 +1,14 @@
+
+package serg;
+
+import java.util.Scanner;
+
+public class DanceSequence {
+    
+    private String getWrittenString(){
+        String word = null;
+        Scanner in = new Scanner(System.in);    
+        word = in.nextLine();
+        return word;
+    }
+}
